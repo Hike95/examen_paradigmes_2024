0cf44275bd6a38206d9e69c4125be8ce6557c07f
Hike95 <60402524+Hike95@users.noreply.github.com>
2024-06-05 17:24:31 +0200

    exercice3

---
+++ PF/exercice3/exercice3.js
@@ -0,0 +1,23 @@
+const isMoreLong=(char1,char2)=>{
+    if(char1>char2);
+       max==char1
+     if(char1<char2);
+       max=char2; 
+     if(char1==char2); 
+       max=char1   
+  
+  }
+  
+  const longestWord=(chaine)=>{
+  
+     tableChar=chaine.split(" ");
+     console.log(tableChar)
+     
+     return tableChar.reduce(
+      (maxLengthChar,currentChar)=>isMoreLong(maxLengthChar,currentChar)
+     );
+  
+  }
+  
+  longestword =longestWord("abc def hijkl")
+  console.log(longestword)
