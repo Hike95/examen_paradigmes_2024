4635201d7c97fba5a6c6adffc69008a9058a0671
Hike95 <60402524+Hike95@users.noreply.github.com>
2024-06-05 15:49:14 +0200

    Exercice1 PF

---
+++ exercice1/ex01.js
@@ -0,0 +1,28 @@
+const calculateTip((facture, note) => {
+    if (note === "Terrible");//arr.splice(2, 1);
+       pourboire=facture
+    if (note === "Poor");
+       pourboire=facture*0.05  
+
+    if (note === "Good");
+
+       pourboire=facture*0.1
+    if (note === "Great");
+
+       pourboire=facture*0.15
+    if (note === "ExcellEnt");
+       pourboire=facture*0.2
+    if ((note != "ExcellEnt")&&(note != "Great")&&(note != "Good")&&(note != "Poor")&&(note != "Terrible"));
+       pourboire=pourboire
+       console.log("Rating not recognised") 
+
+
+    return pourboire;
+
+
+
+});
+
+calculateTip(20, "ExcellEnt");
+calculateTip(26.95, "goOd");
+
